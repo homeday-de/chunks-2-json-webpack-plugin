88aaa196a046bee03a8aead3b60674802f0eb193
sinisag <sinisa@grubor.si>
2019-02-12 18:44:13 +0100

    :recycle:

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const defaultOptions = {
 
 class Chunks2JsonWebpackPlugin {
     constructor(options) {
+        // overwrite default options
         this.options = Object.assign({}, defaultOptions, options);
         this.result = {};
     }
@@ -24,29 +25,30 @@ class Chunks2JsonWebpackPlugin {
         compiler.hooks.emit.tap(pluginName, compilation => {
             this.result = {};
             compilation.chunks.forEach(chunk => {
-                if (!this.result[chunk.name]) {
+                if (this.result[chunk.name] === undefined) {
                     this.result[chunk.name] = {};
                 }
                 chunk.files.forEach(filename => {
-                    const exclude = typeof this.options.excludeFile === 'function' ?
-                        this.options.excludeFile(filename, chunk) :
-                        this.options.excludeFile.test(filename);
-                    if (!exclude) {
-                        const ext = this.options.chunkGroupName(filename, chunk);
-                        if (!this.result[chunk.name][ext]) this.result[chunk.name][ext] = [];
-                        this.result[chunk.name][ext].push(this.options.publicPath + filename);
+                    if (this._excludeChunk(this.options.excludeFile, filename, chunk) === true) {
+                        return;
                     }
+                    const ext = this.options.chunkGroupName(filename, chunk);
+                    if (this.result[chunk.name][ext] === undefined) {
+                        this.result[chunk.name][ext] = [];
+                    }
+                    this.result[chunk.name][ext].push(`${this.options.publicPath}${filename}`);
                 });
             });
             this.saveJson();
         });
     }
     saveJson() {
-        const projectRoot = process.cwd();
         // try to create outputDir folder if it is within project root
-        if (this.options.outputDir.startsWith(projectRoot) || !this.options.outputDir.startsWith('/')) {
-            let pathStep = projectRoot;
-            this.options.outputDir.replace(/^\.\//, '').replace(projectRoot, '').split('/').forEach((folder) => {
+        if (this._shouldFolderBeCreated(this.options.outputDir) === true) {
+            let pathStep = process.cwd();
+            // remove relative prefix
+            const normalizedOutputPath = this._normalizeOutputDir(this.options.outputDir);
+            normalizedOutputPath.split('/').forEach((folder) => {
                 pathStep = path.join(pathStep, folder);
                 try {
                     fs.mkdirSync(pathStep);
@@ -64,6 +66,49 @@ class Chunks2JsonWebpackPlugin {
             console.error(e);
         }
     }
+
+    /**
+     * We might want to skip some entries
+     * this function checks users criteria and returns a bool
+     * @param Function|RegEx excludeCriterium - passed option to skip some entries
+     */
+    _excludeChunk(excludeCriterium, filename, chunk) {
+        if (typeof excludeCriterium === 'function') {
+            return excludeCriterium(filename, chunk);
+        }
+        if (excludeCriterium instanceof RegExp) {
+            return excludeCriterium.test(filename);
+        }
+       // if wrong criteria is passed, we're gonan include the file by default
+        return true;
+    }
+    /**
+     * We need to make sure, that we can actually create the folder.
+     * We can do so, if the desired output is inside project root
+     * @param String outputDir - path to output directory
+     */
+    _shouldFolderBeCreated(outputDir) {
+        // this returns absolute path
+        // handle absolute path, that points to project
+        const isAbsolutePathWithProjectRoot = outputDir.includes(process.cwd());
+        // if output is inside the folder, we're all good
+        const isPathWithinProjectRoot = !outputDir.startsWith('/');
+        return isAbsolutePathWithProjectRoot ||
+        isPathWithinProjectRoot ||
+        false;
+    }
+    /**
+     * To create output folder, we need to understand,
+     * which folders to create. This function normalizes
+     * relative and absolute path, to output we can then
+     * work with - e.g.: folder1/folder2/folder3
+     * @param String outputDir - path to outpurDirectory
+     */
+    _normalizeOutputDir(outputDir) {
+        const removedRelativePrefix = outputDir.replace(/^\.\//, '');
+        const removeAbsolutePrefix = removedRelativePrefix.replace(process.cwd(), '');
+        return removeAbsolutePrefix;
+    }
 }
 
-module.exports = Chunks2JsonWebpackPlugin; 
+module.exports = Chunks2JsonWebpackPlugin;
